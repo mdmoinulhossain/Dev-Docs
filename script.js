b0a75f7ff92f3e6d1b9dbde04733ae93e8398815
mdmoinulhossain <mdmoinulhossain100@gmail.com>
2022-11-08 12:45:39 +0600

    elements chnages

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
+  dropdown[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
@@ -13,3 +13,22 @@ for (i = 0; i < dropdown.length; i++) {
     }
   });
 }
+
+// Click and changed Functions
+
+function openContent(evt, itemsName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(itemsName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
